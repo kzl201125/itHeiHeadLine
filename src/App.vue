bc9fc89e6ea044607740491bb5afb08a025df12c
Kang Zhiliang <2363426384@qq.com>
2022-05-17 12:48:55 +0800

    项目初始化-调整初始目录结构-导入图片素材

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view />
+    <div>
+      <i class="ithei ithei-lishi"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="less"></style>
